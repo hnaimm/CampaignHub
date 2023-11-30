5d2b4d835a9e2dfea8d79d388f438130b5a9b97e
Haifa <hnaimm0@gmail.com>
2023-11-30 15:49:46 +0200

    password input type

--- src/app/Home/SignInForm.tsx
+++ src/app/Home/SignInForm.tsx
@@ -86,6 +86,7 @@ const SignInForm = ({ modalMethods }: { modalMethods: any }) => {
           source="password"
           label="Password"
           placeholder="••••••••••"
+          type="password"
           iconURL="data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' height='16' width='14' viewBox='0 0 448 512' fill='%238180a6' %3E%3Cpath d='M144 144v48H304V144c0-44.2-35.8-80-80-80s-80 35.8-80 80zM80 192V144C80 64.5 144.5 0 224 0s144 64.5 144 144v48h16c35.3 0 64 28.7 64 64V448c0 35.3-28.7 64-64 64H64c-35.3 0-64-28.7-64-64V256c0-35.3 28.7-64 64-64H80z' /%3E%3C/svg%3E"
           required
         />
